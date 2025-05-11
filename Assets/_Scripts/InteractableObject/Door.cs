c90ca43bde13cc3f575c19a4b3ece7e8478aa673
CLEM\konst <konstantytadeusz@gmail.com>
2025-05-11 05:06:54 +0200

    added fadeout and fade in effect for entering rooms

--- Assets/_Scripts/InteractableObject/Door.cs
+++ Assets/_Scripts/InteractableObject/Door.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 
 
@@ -9,6 +11,13 @@ public class Door :InteractiveObject
     {
         base.OnObjectInteracted();
         gameObjectToLoad.SetActive(true);
+        gameObjectToLoad.GetComponent<CanvasGroup>().DOFade(1.0f, 0.2f).SetDelay(1.8f);
+        Tween fadeout = thisGameObject.GetComponent<CanvasGroup>().DOFade(0.0f, 0.2f).SetDelay(0.15f);
+        StartCoroutine(WaitForCanvasToFadeOut(fadeout));
+    }
+    IEnumerator WaitForCanvasToFadeOut(Tween tween)
+    {
+        yield return new DOTweenCYInstruction.WaitForCompletion(tween);
         thisGameObject.SetActive(false);
     }
 }
--- Assets/_Scripts/InteractableObject/ReturnToPreviousRoom.cs
+++ Assets/_Scripts/InteractableObject/ReturnToPreviousRoom.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class ReturnToPreviousRoom : InteractiveObject
 {
@@ -16,6 +18,15 @@ public class ReturnToPreviousRoom : InteractiveObject
     protected override void OnObjectInteracted(){
         base.OnObjectInteracted();
         gameObjectToReturnTo.SetActive(true);
+        gameObjectToReturnTo.GetComponent<CanvasGroup>().DOFade(1.0f, 0.2f).SetDelay(1.8f);
+        //thisGameObject.SetActive(false);
+        Tween fadeout = thisGameObject.GetComponent<CanvasGroup>().DOFade(0.0f, 0.2f).SetDelay(0.15f);
+        StartCoroutine(WaitForCanvasToFadeOut(fadeout));
+    }
+
+    IEnumerator WaitForCanvasToFadeOut(Tween tween)
+    {
+        yield return new DOTweenCYInstruction.WaitForCompletion(tween);
         thisGameObject.SetActive(false);
     }
 }
