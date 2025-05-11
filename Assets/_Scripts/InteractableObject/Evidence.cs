32a297301dc09957768f9ca945157378d03f5645
Ognaseon <Konradbzymek12@gmail.com>
2025-05-11 11:29:11 +0200

    Added Findable Objects

--- Assets/_Scripts/InteractableObject/Evidence.cs
+++ Assets/_Scripts/InteractableObject/Evidence.cs
@@ -5,11 +5,13 @@ public class Evidence : InteractiveObject
 {
     [SerializeField] EvidenceObject evidenceObject;
     [SerializeField] StringEvent uIText;
+    [SerializeField] GameObject thisGameObject;
     [SerializeField] string messege = "Znalazłeś:";
     protected override void OnObjectInteracted()
     {
         base.OnObjectInteracted();
         evidenceObject.status = EvidenceStatus.UnExplored;
         uIText.TextValue = $@"{messege} {evidenceObject.name}";
+        thisGameObject.SetActive(false);
     }
 }
